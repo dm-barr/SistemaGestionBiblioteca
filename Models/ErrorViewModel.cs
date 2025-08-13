71024215fdee0b929deb77a21f13a37ff40d016b
dm-barr <dianabarrantesga1510@gmail.com>
2025-08-12 22:04:23 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaGestionBiblioteca.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
